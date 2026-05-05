e0043e65214eaca74adcb6ce1ce7753deadeb59e
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2026-05-05 19:12:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstChar(text) {
   // your code here
+	for(let i=0; i<text.length; i++){
+		if(text.charAt[i]==' '){
+			continue;
+		}else{
+			return text.charAt[i];
+		}
+	}
+	return ' ';
 }
 
 // Do not change the code below
